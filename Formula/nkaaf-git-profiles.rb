cb67b45824a022fc70c5889a544022dcb24896dc
nkaaf <nkaaf@protonmail.com>
2025-10-17 23:24:32 +0200

    git-profiles 0.1.0 (new formula)
    Closes #1.
    
    Signed-off-by: Niklas Kaaf <37747079+nkaaf@users.noreply.github.com>

---
+++ Formula/nkaaf-git-profiles.rb
@@ -0,0 +1,51 @@
+class NkaafGitProfiles < Formula
+  include Language::Python::Virtualenv
+
+  desc "Managing Git profiles systemwide"
+  homepage "https://github.com/nkaaf/git-profiles"
+  url "https://github.com/nkaaf/git-profiles/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "53b291eadaba0084b25473bfc4ef42b803bc174d893c31ec57eb7c5c1740ed92"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build # for pydantic-core
+  depends_on "python@3.14"
+
+  resource "annotated-types" do
+    url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
+    sha256 "aff07c09a53a08bc8cfccb9c85b05f1aa9a2a6f23728d790723543408344ce89"
+  end
+
+  resource "platformdirs" do
+    url "https://files.pythonhosted.org/packages/61/33/9611380c2bdb1225fdef633e2a9610622310fed35ab11dac9620972ee088/platformdirs-4.5.0.tar.gz"
+    sha256 "70ddccdd7c99fc5942e9fc25636a8b34d04c24b335100223152c2803e4063312"
+  end
+
+  resource "pydantic" do
+    url "https://files.pythonhosted.org/packages/f3/1e/4f0a3233767010308f2fd6bd0814597e3f63f1dc98304a9112b8759df4ff/pydantic-2.12.3.tar.gz"
+    sha256 "1da1c82b0fc140bb0103bc1441ffe062154c8d38491189751ee00fd8ca65ce74"
+  end
+
+  resource "pydantic-core" do
+    url "https://files.pythonhosted.org/packages/df/18/d0944e8eaaa3efd0a91b0f1fc537d3be55ad35091b6a87638211ba691964/pydantic_core-2.41.4.tar.gz"
+    sha256 "70e47929a9d4a1905a67e4b687d5946026390568a8e952b92824118063cee4d5"
+  end
+
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
+    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
+  end
+
+  resource "typing-inspection" do
+    url "https://files.pythonhosted.org/packages/55/e3/70399cb7dd41c10ac53367ae42139cf4b1ca5f36bb3dc6c9d33acdb43655/typing_inspection-0.4.2.tar.gz"
+    sha256 "ba561c48a67c5958007083d386c3295464928b01faa735ab8547c5692e87f464"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    output = shell_output("#{bin}/git-profiles --help")
+    assert_match "usage:", output
+  end
+end
